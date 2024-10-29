5be89329367ac0639a03045126c09a96a25cb6fc
Stéphane Caron <stephane.caron@inria.fr>
2024-10-29 11:48:20 +0100

    WIP: Prepare __init__.py

---
+++ uplot/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 Stéphane Caron
+# Copyright 2023-2024 Inria
+
+"""Plot Python iterables with µPlot."""
+
+__version__ = "1.0.0"
+
+from .plot import plot
+
+__all__ = ["plot"]
