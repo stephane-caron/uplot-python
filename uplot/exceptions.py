fd6f7b5853de03920ebbe529b462dda534abd776
Stéphane Caron <stephane.caron@inria.fr>
2024-10-29 11:48:11 +0100

    WIP: Add color picker and exceptions

---
+++ uplot/exceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 Stéphane Caron
+# Copyright 2024 Inria
+
+"""Project exceptions."""
+
+
+class UplotException(Exception):
+    """Base class for uPlot exceptions."""
