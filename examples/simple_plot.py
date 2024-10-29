79554d7256979a2c16e35500818dcf526b11664a
Stéphane Caron <stephane.caron@inria.fr>
2024-10-29 15:36:52 +0100

    WIP: Add labels to example

--- examples/simple_plot.py
+++ examples/simple_plot.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     t = np.arange(10)
     uplot.plot2(
         t,
-        [np.exp(0.1 * t), np.exp(-10 * t), np.cos(t)],
+        [np.exp(0.1 * t), np.exp(-10.0 * t), np.cos(t)],
         title="Simple plot",
         time=False,
+        left_labels=["exp(0.1 t)", "exp(-K t)", "cos(t)"],
     )
